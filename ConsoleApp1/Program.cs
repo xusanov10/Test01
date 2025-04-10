a23fdc7edc40b05d3bb0e960227ed543a4b04081
xusanov10 <ahmadjonxusanovv>
2025-04-10 15:17:56 +0500

    change 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -60,3 +60,38 @@ static void Main3(string[] args)
     }
 
 }
+
+static void Main5(string[] args)
+{
+    int[,] ints = new int[2, 2];
+
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+        {
+
+            Random rnd = new Random();
+            ints[i, j] = rnd.Next(1, 20);
+
+        }
+    }
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+        {
+            Console.Write(ints[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        int counter = 0;
+        for (int j = 0; j < ints.GetLength(1); j++)
+        {
+            counter += ints[i, i];
+        }
+        Console.Write(counter + " ");
+    }
+}

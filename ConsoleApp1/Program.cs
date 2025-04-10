34ac979a89294f38a23e2cf8cc9879588d50514a
xusanov10 <ahmadjonxusanovv>
2025-04-10 14:52:07 +0500

    change1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+   
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5 };
+            int maxs = 0;
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] > maxs) maxs = ints[i];
+
+            }
+            Console.WriteLine(maxs);
+        }
+
+  
